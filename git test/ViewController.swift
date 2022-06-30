619030e52df7eb83bf1f63c760fc1cb28923ac85
Debjit-micronixsystem2019 <debjit.micronixsystem@gmail.com>
2022-06-30 14:48:21 +0530

    Initial Commit

---
+++ git test/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  git test
+//
+//  Created by admin on 6/30/22.
+//  Copyright © 2022 Debjit Jana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("abc")
+    }
+
+
+}
+
---
+++ git test/newFile.swift
@@ -0,0 +1,13 @@
+//
+//  newFile.swift
+//  git test
+//
+//  Created by admin on 6/30/22.
+//  Copyright © 2022 Debjit Jana. All rights reserved.
+//
+
+import Foundation
+
+class abc {
+    var ab = Int()
+}
